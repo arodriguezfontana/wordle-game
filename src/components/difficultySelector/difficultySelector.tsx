f21b36b32d9abb598e3b8eddf23a76763da815c8
Abril Rodríguez <arodriguezfontana@gmail.com>
2025-07-13 17:10:27 -0300

    estilos difficulty terminado

--- src/components/difficultySelector/difficultySelector.tsx
+++ src/components/difficultySelector/difficultySelector.tsx
@@ -1,41 +1,40 @@
+import './difficultySelector.css';
 import Loading from "../loading/loading";
 import Button from "../button/button";
 import type { Difficulty } from "../../types/difficulty";
 
 interface DifficultySelectorProperties {
-    difficulties: Difficulty[];
-    onSelect: (difficulty: Difficulty) => void;
-    loading: boolean;
-};
+  difficulties: Difficulty[];
+  onSelect: (difficulty: Difficulty) => void;
+  loading: boolean;
+}
 
 const DifficultySelector = ({ difficulties, onSelect, loading }: DifficultySelectorProperties) => {
-    return (
-        <div>
-            <h3>Seleccioná la dificultad</h3>
+  return (
+    <div className="difficulty-selector-container">
+      <h3 className="difficulty-title">Seleccioná la dificultad</h3>
 
-            {loading && (
-                <Loading />
-            )}
+      {loading && <div className="loading-wrapper"><Loading /></div>}
 
-            {!loading && (
-                <ul>
-                    {difficulties.map((d, i) => {
-                        const colors = ["green", "yellow", "orange", "red"];
-                        const color = colors[i] || "gray";
-
-                        return (
-                            <li key={d.id}>
-                                <Button onClick={() => onSelect(d)} color={color}>
-                                    {d.name}
-                                </Button>
-                            </li>
-                        );
-                    })}
-                </ul>
-
-            )}
-        </div>
-    );
+      {!loading && (
+        <ul className="difficulty-list">
+          {difficulties.map((d, i) => {
+            const colors = ["#22c55e", "#facc15", "#fb923c", "#ef4444"];
+            const color = colors[i] || "#6b7280";
+            return (
+              <li key={d.id}>
+                <div className='difficulty-buttons'>
+                  <Button onClick={() => onSelect(d)} color={color}>
+                    {d.name}
+                  </Button>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </div>
+  );
 };
 
 export default DifficultySelector;
